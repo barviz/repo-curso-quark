7097bca8ef9b98d2d9a73d92ae93ffbebd6b9b44
barviz <barbaravizgarra@gmail.com>
2023-04-23 21:46:30 -0300

    ejercicio32

---
+++ 01-C++ Elemental/32WhileAlturaPromedio/32WhileAlturaPromedio/32WhileAlturaPromedio.cpp
@@ -0,0 +1,32 @@
+/*Se ingresan un conjunto de n alturas de personas por teclado.
+Mostrar la altura promedio de las personas.*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, x;
+    float altura, suma, promedio;
+
+    cout << "Cuantas personas hay: ";
+    cin >> n;
+
+    x = 1;
+    suma = 0;
+
+    while (x <= n)
+    {
+        cout << "Ingrese la altura: ";
+        cin >> altura;
+        suma = suma + altura;
+        x = x + 1;
+    }
+
+    promedio = suma / n;
+    cout << "Altura promedio: ";
+    cout << promedio;
+
+    return 0;
+}
